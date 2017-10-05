855e393b2efe4f5a6f3e366bd74cd71b3a9991b9
samsuite <suitester@gmail.com>
2017-10-04 21:28:42 -0400

    improvements and scene setup

--- Assets/Code/CollisionPrediction.cs
+++ Assets/Code/CollisionPrediction.cs
@@ -8,6 +8,7 @@ public class CollisionPrediction : MonoBehaviour {
     AgentMovement my_agent;
     AgentMovement[] all_agents;
     const float collision_distance = 0.5f;
+    const float vision_range = 1f;
 
     void Start () {
         my_agent = GetComponent<AgentMovement>();
@@ -17,16 +18,18 @@ public class CollisionPrediction : MonoBehaviour {
     public void Update () {
         for (int i = 0; i < all_agents.Length; i++) {
             if (all_agents[i] != my_agent) {
-                if (Vector3.Dot(get_position_at_time_of_approach_to(all_agents[i])-transform.position, my_agent.rb.velocity) > 0) {
-                    if (get_distance_at_time_of_approach_to(all_agents[i]) <= collision_distance) {
+                if (Vector3.Distance(all_agents[i].transform.position, transform.position) <= vision_range) {
+                    if (Vector3.Dot(get_position_at_time_of_approach_to(all_agents[i])-transform.position, my_agent.rb.velocity) > 0) {
+                        if (get_distance_at_time_of_approach_to(all_agents[i]) <= collision_distance) {
 
-                        Vector2 vectorDirection = all_agents[i].transform.position - transform.position;
-			            Vector2 forwardDirection = transform.up;
+                            Vector2 vectorDirection = all_agents[i].transform.position - transform.position;
+			                Vector2 forwardDirection = transform.up;
 
-			            Vector3 cross = Vector3.Cross(vectorDirection, forwardDirection);
-			            float sign = Mathf.Sign(cross.z);
+			                Vector3 cross = Vector3.Cross(vectorDirection, forwardDirection);
+			                float sign = Mathf.Sign(cross.z);
 
-                        my_agent.targetDirection = (Quaternion.Euler(0,0,45f * sign) *transform.up).normalized;
+                            my_agent.targetDirection = (Quaternion.Euler(0,0,45f * sign) *transform.up).normalized;
+                        }
                     }
                 }
             }
@@ -69,6 +72,7 @@ public class CollisionPrediction : MonoBehaviour {
         return ((my_agent.transform.position + (u*time_at_cpa)) - (other_agent.transform.position + (v*time_at_cpa))).magnitude;
     }
 
+    /*
     void OnDrawGizmos () {
         for (int i = 0; i < all_agents.Length; i++) {
             if (all_agents[i] != my_agent) {
@@ -79,6 +83,6 @@ public class CollisionPrediction : MonoBehaviour {
                 }
             }
         }
-    }
+    }*/
 
 }
